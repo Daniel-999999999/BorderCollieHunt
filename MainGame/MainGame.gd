0856a94314dc804b5d83e9e1211ef08b929a77f8
krishna <0186359@schoolsnet.act.edu.au>
2022-03-08 15:15:25 +1100

    a lto of things added
    more enemies, shooting, timer, score, working on bullet border deleter, need to add a leaderboard

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,12 +1,20 @@
 extends Control
 
+export(int) var countDownMax
+var currentTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_process(true)
-
-func _process(delta):
-	pass
+	currentTimer = countDownMax
+	$HUD/Countdown.text = str(currentTimer)
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
+		currentTimer = currentTimer -1
+		print(currentTimer)
+	print("GAME OVER")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Player/Player.gd
+++ Player/Player.gd
@@ -14,10 +14,17 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	pass
-			
+	if Input.is_action_pressed("ui_down"):
+		move_and_collide(Vector2(0, movement_speed * delta))
+	if Input.is_action_pressed("ui_up"):
+		move_and_collide(Vector2(0, -movement_speed * delta))
+		
 func _process(delta):
-	pass
+	if Input.is_action_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+# warning-ignore:standalone_expression
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
